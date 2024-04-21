d6676d138aab888f395540e09c805f3bf2ff6c2b
UsatovPavel <143062791+UsatovPavel@users.noreply.github.com>
2024-04-21 23:00:56 +0300

    Spawn Generals demo
    Поверх тайлов мапы с генералами заспавнил BP-наследника класса AStrategist
    Туман войны не поддерживается пока

---
+++ Spawn BP_Generals/Source/Voevoda/GameWorld.cpp
@@ -0,0 +1,48 @@
+#include "GameWorld.h"
+#include "CoreMinimal.h"
+#include "EngineUtils.h"
+#include "GameFramework/Actor.h"
+#include "Templates/SharedPointer.h"
+
+
+AGameWorld::AGameWorld() {
+
+    PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AGameWorld::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+}
+
+void AGameWorld::BeginPlay() {
+    Super::BeginPlay();
+
+    for (TActorIterator<AMapPainter> ActorItr(GetWorld()); ActorItr;
+        ++ActorItr) {
+
+        painter_ptr = Cast<AMapPainter>(*ActorItr);
+        map_ptr = &(painter_ptr->map);
+    }
+
+    if (painter_ptr) {
+        //AStrategist* Spawned_strateg =
+        //GetWorld()->SpawnActor<AStrategist>(AStrategist::StaticClass(), FVector(100.f, 100.f, 0.f), FRotator::ZeroRotator);
+        //TWeakObjectPtr<AStrategist>(Spawned_strateg));
+        for (auto& general_pos : map_ptr->GeneralsInitPos) {
+            AActor* Spawned_strateg =
+            GetWorld()->SpawnActor<AActor>(BP_Strategist, general_pos.UE_coordinates(), FRotator::ZeroRotator);
+            strategists.Add(Cast<AStrategist>(Spawned_strateg));
+            //TSharedPtr<General> Strategist_ptr(new General());//work
+            //TSharedPtr<AStrategist> (MakeShareable(AStrategist(general_pos)));//not work
+            //strategists.Add(&new_strat1egist);
+        }
+        for (auto& castle_pos : map_ptr->CastlesInitPos) {
+            //AStructure new_castle(castle_pos);
+            //structures.Add(&new_castle);
+        }
+    }
+    else {
+        UE_LOG(LogTemp, Warning, TEXT("painter_ptr is nullptr."));
+    }
+}
---
+++ Spawn BP_Generals/Source/Voevoda/Location.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VOEVODA_API Location
+{
+public:
+	int32 x;
+	int32 y;
+	Location(): x(0), y(0){ }
+	Location(const int32 x_, const int32 y_):x(x_), y(y_) {}
+	FVector UE_coordinates() {
+		return FVector(64.f * x+32, -64.f * y-32, 1.f);
+	}
+};
